ddd2627e277883cabd9a649c707f41f400e5c783
Malidu Pahasara <malidupahasara04@gmail.com>
2024-11-19 01:52:57 +0530

    auth changes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,7 @@
-import User from "";
-
-
-
+import User from "..models/User.js";
+import bcrypt from "bcryptjs";
+import { createError } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 
 export const register = async (req, res, next) => {
